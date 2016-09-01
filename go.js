5a0e835b1d9b52b6e4346e01dd0a75bc52a6c95f
sdc <heysdc@163.com>
2016-09-01 19:16:29 +0800

    es5

--- go.js
+++ go.js
@@ -1,11 +1,11 @@
-const name = '孙大超'
+var name = '孙大超'
 
 function test () {
-  setTimeout(() => {
+  setTimeout(function () {
     var input = document.getElementsByClassName('option')[0]
     input.value = name
   }, 2000)
-  setTimeout(() => {
+  setTimeout(function () {
     var btn = document.getElementById('next_button')
     btn.click()
   }, 3000)
--- orderFood.js
+++ orderFood.js
@@ -1,9 +1,9 @@
 /* global chrome */
-const foodUrl = 'https://www.wenjuan.com/s/Vf6Vze/'
-const orderFoodHour = 12
-const interval = 3600000
+var foodUrl = 'https://www.wenjuan.com/s/Vf6Vze/'
+var orderFoodHour = 12
+var interval = 3600000
 
-setInterval(() => {
+setInterval(function () {
   var a = new Date()
   if (a.getHours() === orderFoodHour && a.getDay() !== 0 && a.getDay() !== 6) {
     chrome.tabs.create({
