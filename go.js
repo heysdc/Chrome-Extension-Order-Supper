6801174f61983a7883b33b385f1516da8b7b4e2d
sdc <heysdc@163.com>
2016-09-02 00:53:43 +0800

    ifWrong

--- go.js
+++ go.js
@@ -3,6 +3,10 @@ var name = '孙大超'
 function test () {
   setTimeout(function () {
     var input = document.getElementsByClassName('option')[0]
+    if (!input) {
+      window.alert('-_-#订餐出了点问题，自己订吧')
+      return false
+    }
     input.value = name
   }, 2000)
   setTimeout(function () {
