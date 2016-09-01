7ae35c6ef9211d898731d4307136af415279b261
sdc <heysdc@163.com>
2016-09-02 01:55:34 +0800

    autoClose

--- orderFood.js
+++ orderFood.js
@@ -2,6 +2,18 @@
 var foodUrl = 'https://www.wenjuan.com/s/Vf6Vze/'
 var orderFoodHour = 12
 var interval = 3600000
+var closeTime = 5000
+
+var closeWindow = function () {
+  setTimeout(function () {
+    chrome.tabs.query(
+      {currentWindow: true, active: true},
+      function (tabArray) {
+        chrome.tabs.remove(tabArray[tabArray.length - 1].id)
+      }
+    )
+  }, closeTime)
+}
 
 setInterval(function () {
   var a = new Date()
@@ -12,6 +24,7 @@ setInterval(function () {
       chrome.tabs.executeScript(null, {
         file: 'go.js'
       })
+      closeWindow()
     })
   }
 }, interval)
